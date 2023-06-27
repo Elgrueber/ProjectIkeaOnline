3d1752b109fad4b1e21a1bdd6d81a0a3f2876893
Elgrueber <elenagrueber@gmail.com>
2023-06-27 00:21:06 -0400

    Some commit

--- src/test/java/tests/testFindSamples.java
+++ src/test/java/tests/testFindSamples.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import models.*;
 
@@ -13,11 +14,24 @@ import java.util.List;
 public class testFindSamples extends BaseTest {
 
 
-        @Test
-        public void testFindSample() {
-            HomePage homePage = new HomePage(getDriver())
-                    .clickHamburgerMenu();
-            }
-        }
+    @Test
+    public void testFindSample() {
+        HomePage homePage = new HomePage(getDriver())
+                .clickHamburgerMenu();
+    }
 
+    @Ignore
+    @Test
+    public void testFindSampleBed() {
+        final String productBedMalma = "High bed frame/2 storage boxes, black-brown/Luröy,";
+        MainPage mainPage = openBaseURL()
+                .clickOnFurnitureFromProduct()
+                .clickOnBed()
+                .clickOnBedWithStorage()
+                .clickOnBedMalma();
+
+        MalmBedPage malmBedPage = new MalmBedPage(getDriver());
+        Assert.assertEquals(malmBedPage.getTextMessage(), productBedMalma);
+    }
+}
 
